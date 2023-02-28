0dd5f9e75788762ce075e635aa444c7d607a2679
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-02-28 22:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+document.querySelector("form").addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	let name = document.querySelector("#name").value;
+	let year = document.querySelector("#year").value;
+
+	document.querySelector("h3").textContent=`https://localhost:8080/?name=${name}&year=${year}`;
+});
